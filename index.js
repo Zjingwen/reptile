edfbd51a336ac7ba1967842886b2cae892d150a9
Zjingwen <309940379@qq.com>
2016-11-12 20:12:14 +0800

    2016.11.12 爬取知乎专题，保存到本地

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 'use strict';
 
-var zhihu = require("zhihu"),
-	Question = require("zhihu").Question,
-	Collection = require("zhihu").Collection;
-
-var htmlEscape = require("./htmlEscape/index.js").htmlEscape;
-
-var fs = require("fs"),
-	Thenjs = require('thenjs');
+var zhihu = require("./requests/api.js").zhihu;
+var fs = require("fs");
 
 var readline = require('readline');
 
-// var User = {
-// 	name : "jing-wen-zhou",
-// 	collection:{
-// 		url:"https://www.zhihu.com/collection/42474270"
-// 	}
-// }
-
 /**
- * 读取知乎收藏文件夹
+ * [rl 命令行输入框]
  */
-var  rl = readline.createInterface({
+/*var  rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 });
@@ -37,53 +24,40 @@ rl.question("知乎专题id是多少？",function(answer){
 	}
     collectionFun(User);
     rl.close();
-});
+});*/
 
-function collectionFun(User){
-	console.log("正在爬取");
-	Collection.getPagination(User.collection.url).then(function(info){
-		console.log('当前收藏一共：'+info.pages+'页');
-		return info;
-	}).then(function(index){
-		var i = 1;
-		var time = function(){
-			setTimeout(function(){
-				collection (i++);
-				if ( i <= index.pages ) time();
-			},1000);
-		}
-		time();
+function collection(id,page){
+	var page = ++page;
+	zhihu.GetCollection(id,page).then(function(value){
+		console.log("----- 第"+page+"页已获取 -----");
+		return value;
+	}).then(function(value){
+		value.forEach(function(value){
+			var item = {
+				title:value.title,
+				content:value.body
+			};
+			io(item.title,item.content);
+		})
+		setTimeout(function() {
+			if(value[0]) collection(id,page);
+		},1000);
 	});
-
-	function collection (index){
-		var url = User.collection.url+'?page='+index;
-		Collection.getDataByPage(url).then(function(info){
-			return info
-		}).then(function(value){
-			value.forEach(function(value){
-				value.content = htmlEscape(value.content);
-				var item = {
-					title:value.question.title,
-					content:value.content
-				}
-				io(item.title,item.content);
-			})
-		});
-	}
-
-	function io(value,content){
-		fs.open('data/'+value+'.md','w',function(err,data){
-			if(err){
-				console.log("文件创建失败");
-				return false;
-			}
-		})
-		fs.writeFile('data/'+value+'.md',content,function(err,data){
-			if(err){
-				console.log("文件写入失败");
-				return console.error(err);
-			}
-			console.log(value+".md"+"-----文件写入成功");
-		})
-	}
 }
+function io(title,content){
+	fs.open('zhihu/'+title+'.md','w',function(err,data){
+		if(err){
+			console.log("文件创建失败");
+			return false;
+		}
+	})
+	fs.writeFile('zhihu/'+title+'.md',content,function(err,data){
+		if(err){
+			console.log("文件写入失败");
+			return console.error(err);
+		}
+		console.log(title+".md"+"-----文件写入成功");
+	})
+}
+
+collection('42474270',0);
