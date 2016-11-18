1ad2254b2cb4428fd34b4921a40c32b5d476ac78
Zjingwen <309940379@qq.com>
2016-11-18 18:20:44 +0800

    2016.11.18 添加standard效验代码

--- htmlAnalyse/index.js
+++ htmlAnalyse/index.js
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
-var config = require("../htmlAnalyse/config.js").config;
+var config = require('../htmlAnalyse/config.js').config
 
-var htmlAnalyse = function(value){
-	if(!value){
-		console.log("没有内容");
-		return false;
-	}
-	config.forEach(function(val){
-		var pattern = val[0];
-		var rep = new RegExp(pattern,'g');
-		value = value.replace(rep,val[1]);
-	})
+var htmlAnalyse = function (value) {
+  if (!value) {
+    console.log('没有内容')
+    return false
+  }
+  config.forEach(function (val) {
+    var pattern = val[0]
+    var rep = new RegExp(pattern, 'g')
+    value = value.replace(rep, val[1])
+  })
 
-	value = value.replace(/\/\/link.zhihu.com\/\?target=/g,'');
-	value = value.replace(/target="_blank"/g,'');
-	value = value.replace(/class=".*?"/g,'');
-	value = value.replace(/rel=".*?"/g,'');
-	value = value.replace(/\n/g,'<br />');
-	value = value.replace(/data-original=".*?"/g,'');
-	value = value.replace(/data-rawwidth=".*?"/g,'');
-	value = value.replace(/data-rawheight=".*?"/g,'');
-	value = value.replace(/data-rawwidth=".*?"/g,'');
-	value = value.replace(/width=".*?"/g,'');
-	
-	return value;
+  value = value.replace(/\/\/link.zhihu.com\/\?target=/g, '')
+  value = value.replace(/target="_blank"/g, '')
+  value = value.replace(/class=".*?"/g, '')
+  value = value.replace(/rel=".*?"/g, '')
+  value = value.replace(/\n/g, '<br />')
+  value = value.replace(/data-original=".*?"/g, '')
+  value = value.replace(/data-rawwidth=".*?"/g, '')
+  value = value.replace(/data-rawheight=".*?"/g, '')
+  value = value.replace(/data-rawwidth=".*?"/g, '')
+  value = value.replace(/width=".*?"/g, '')
+
+  return value
 }
 module.exports = {
-	htmlAnalyse:htmlAnalyse
+  htmlAnalyse: htmlAnalyse
 }
--- zhihuapi/zhihu/collection.js
+++ zhihuapi/zhihu/collection.js
@@ -1,106 +1,107 @@
-'use strict';
+'use strict'
 
-var Promise = require("bluebird");
-var request =  Promise.promisify(require("request"));
-var cheerio = require("cheerio");
-var htmlAnalyse = require("../../htmlAnalyse/index.js").htmlAnalyse;
-var config = require("./config.js").config;
+var Promise = require('bluebird')
+var request = Promise.promisify(require('request'))
+var cheerio = require('cheerio')
+var htmlAnalyse = require('../../htmlAnalyse/index.js').htmlAnalyse
+var config = require('./config.js').config
 
 /**
  * 获取收藏内容
  * @param {string} id   收藏页面id
  * @param {string} page 页码
 */
-function GetCollection(id,page){
-	if(!id) {
-		console.log("你确定id输入了？")
-		return false
-	};
-	var page = page || '';
-	var zhihu = {
-		url : config.collection+id,
-		headers : config.headers
-	};
-	if(page) {
-		zhihu.url = zhihu.url + '?page='+page;
-	}
+function GetCollection (id, page) {
+  if (!id) {
+    console.log('你确定id输入了？')
+    return false
+  }
+  page = page || ''
+  var zhihu = {
+    url: config.collection + id,
+    headers: config.headers
+  }
+  if (page) {
+    zhihu.url = zhihu.url + '?page=' + page
+  }
 
-	function addTitle(title,body){
-		var content = '<h1>'+title+'</h1>'+body;
-		return content;
-	}
-	
-	return request(zhihu).then(function (content) {
-		var $ = cheerio.load(content.body,{decodeEntities: false});
-		var item = [];
-		$(".zm-item").each(function(index,element){
-			/**
-			 * 判断当前类型
-			 * @type {
-			 * type == Answer 当前为回答
-			 * type == Post   当前为专题
-			 * }
-			 */
-			var type = $(element).data('type');
-			/**
-			 * 判断当前问题有没有被和谐
-			 * @type {bool}
-			 */
-			var deleteAswer = Boolean($(element).find('#answer-status').html());
+  function addTitle (title, body) {
+    var content = '<h1>' + title + '</h1>' + body
+    return content
+  }
 
-			if( deleteAswer ){
-				var title = '「被和谐了」'+$(element).find(".zm-item-title a").text();
-				var body = addTitle(title,htmlAnalyse($(element).find("#answer-status").text()));
-				var name = $(element).find(".zm-item-fav .zm-item-answer").data("created");
-			}else{
-				if(type == 'Answer'){
-					var title = $(element).find(".zm-item-title a").text();
-					var body = addTitle(title,htmlAnalyse($(element).find(".zm-item-fav .zm-item-answer .content").text()));
-					var name = $(element).find(".zm-item-fav .zm-item-answer").data("created");
-				}
+  return request(zhihu).then(function (content) {
+    var $ = cheerio.load(content.body, {decodeEntities: false})
+    var item = []
+    $('.zm-item').each(function (index, element) {
+      /**
+       * 判断当前类型
+       * @type {
+       * type == Answer 当前为回答
+       * type == Post   当前为专题
+       * }
+       */
+      var type = $(element).data('type')
+      /**
+       * 判断当前问题有没有被和谐
+       * @type {bool}
+       */
+      var deleteAswer = Boolean($(element).find('#answer-status').html())
+      var title, body, name
 
-				if(type == 'Post'){
-					var title = $(element).find(".zm-item-title a").text();
-					var body = addTitle(title,htmlAnalyse($(element).find(".zm-item-fav .zm-item-post .content").text()));
-					var name = $(element).find(".zm-item-fav meta[itemprop=post-url-token]").attr("content");
-				}
-			}
+      if (deleteAswer) {
+        title = '「被和谐了」' + $(element).find('.zm-item-title a').text()
+        body = addTitle(title, htmlAnalyse($(element).find('#answer-status').text()))
+        name = $(element).find('.zm-item-fav .zm-item-answer').data('created')
+      } else {
+        if (type === 'Answer') {
+          title = $(element).find('.zm-item-title a').text()
+          body = addTitle(title, htmlAnalyse($(element).find('.zm-item-fav .zm-item-answer .content').text()))
+          name = $(element).find('.zm-item-fav .zm-item-answer').data('created')
+        }
 
-			item.push({
-				body:body,
-				title:title,
-				name:name
-			});
-		});
-		return item;
-	});
-};
+        if (type === 'Post') {
+          title = $(element).find('.zm-item-title a').text()
+          body = addTitle(title, htmlAnalyse($(element).find('.zm-item-fav .zm-item-post .content').text()))
+          name = $(element).find('.zm-item-fav meta[itemprop=post-url-token]').attr('content')
+        }
+      }
+
+      item.push({
+        body: body,
+        title: title,
+        name: name
+      })
+    })
+    return item
+  })
+}
 
 /**
  * 获取当前收藏页面页码
  * @param {string} id 收藏页面id
  */
-function GetCollectionPages(id){
-	var zhihu = {
-		url : config.collection+id,
-		headers : config.headers
-	};
-	return request(zhihu).then(function (content){
-		var $ = cheerio.load(content.body,{decodeEntities: false});
+function GetCollectionPages (id) {
+  var zhihu = {
+    url: config.collection + id,
+    headers: config.headers
+  }
+  return request(zhihu).then(function (content) {
+    var $ = cheerio.load(content.body, {decodeEntities: false})
 
-		if(Boolean($('.error').html())){
-			console.log('当前收藏不存在');
-			return false;
-		}
-		var borderPagerNext = $(".border-pager").find("span");
-		var pages = {
-			first : borderPagerNext.eq(1).text(),
-			last : borderPagerNext.eq(5).text()
-		}
-		return pages;
-	})
+    if ($('.error').html()) {
+      console.log('当前收藏不存在')
+      return false
+    }
+    var borderPagerNext = $('.border-pager').find('span')
+    var pages = {
+      first: borderPagerNext.eq(1).text(),
+      last: borderPagerNext.eq(5).text()
+    }
+    return pages
+  })
 }
 module.exports = {
-	GetCollection:GetCollection,
-	GetCollectionPages:GetCollectionPages
+  GetCollection: GetCollection,
+  GetCollectionPages: GetCollectionPages
 }
